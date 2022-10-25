90f9223a2d5eff1f932db262e6a9d1d7b9ab0d57
George2f <juraj.pejnovic@2front.co>
2022-10-25 12:06:04 +0200

    fix(app.test): add describe block and fix styling

--- septesting/src/App.test.tsx
+++ septesting/src/App.test.tsx
@@ -2,14 +2,16 @@ import { act, render } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 import App from './App';
 
-test('renders learn react link', async () => {
-  console.log = jest.fn();
-  act(() => {
-    render(
-      <MemoryRouter>
-        <App greeting="Test greeting" />
-      </MemoryRouter>,
-    );
+describe('App', () => {
+  it('renders learn react link', async () => {
+    console.log = jest.fn();
+    act(() => {
+      render(
+        <MemoryRouter>
+          <App greeting="Test greeting" />
+        </MemoryRouter>,
+      );
+    });
+    expect(console.log).toBeCalled();
   });
-  expect(console.log).toBeCalled();
 });
